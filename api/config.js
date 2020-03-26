8b9a959b47a2e432219da1e41f259e47db057e8e
ItaloRFeitosa <italo85199@gmail.com>
2020-03-26 08:13:08 -0300

    dockerfile and docker-compose

---
+++ api/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+    postgres_container: {
+        host :    `${process.env.POSTGRES_HOST}`,
+        database: `${process.env.POSTGRES_DB}`,
+        user:     `${process.env.POSTGRES_USER}`,
+        password: `${process.env.POSTGRES_PASSWORD}`,
+        port: `${process.env.POSTGRES_PORT}`
+    }
+}
--- api/knexfile.js
+++ api/knexfile.js
@@ -1,32 +1,25 @@
 // Update with your config settings.
-
+const config = require('./config')
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite3'
-    },
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './src/database/db.sqlite3'
+  //   },
+  //   migrations: {
+  //     directory: './src/database/migrations'
+  //   },
+  //   useNullAsDefault: true
+  // },
+
+  teste: {
+    client: 'pg',
+    connection: config.postgres_container,
     migrations: {
       directory: './src/database/migrations'
     },
     useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
 
   production: {
